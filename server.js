426ca261fa761dc53c95322ef3e79ed7fc94781f
luisma2391 <156382865+luisma2391@users.noreply.github.com>
2024-05-20 20:19:30 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./api');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api', api);
+
+app.listen(port, function () {
+    console.log("Server is listening at port: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("hello world");
+});
